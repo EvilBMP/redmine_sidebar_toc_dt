cf1034f9bc3d3a1acef3338f5203503d3d6d1794
TriggerAu <dtregoning@outlook.com.au>
2014-10-25 13:11:44 +1100

    updating version to 0.0.3

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_sidebar_toc_dt do
   name 'Redmine Wiki Sidebar TOC plugin'
   author 'TriggerAu Fork from //Twinslash'
   description 'navigation by the header of the anchor links in the wiki sidebar'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/TriggerAu/redmine_sidebar_toc_dt'
 end
